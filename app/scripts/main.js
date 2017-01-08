3a75233cb7274c5f99faaa1de030c197ffd068a4
Nate Hodge <nhodge253@gmail.com>
2017-01-08 00:01:03 -0800

    added fizzbuzz to the js for fun

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+//added fizzbuzz script for fun
+function fizzbuzz (){
+//Count from 1-20 and stop at 20.
+for(var i = 1; i <= 20; i++) {
+//If the number is divisible by 3 & 5, call out 'FizzBuzz'.
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log ('FizzBuzz');
+   }
+//If the number is only divisible by 3, call out 'Fizz'.
+  else if (i % 3 === 0) {
+    console.log ('Fizz');
+  }
+//If the number is only divisible by 5, call out 'Buzz'.
+  else if (i % 5 === 0) {
+    console.log ('Buzz');
+  }
+//If the number is not divisible by 3 or 5, just call out the number.
+  else {
+   console.log(i);
+}
+
+}
+}
+
+fizzbuzz ();
